86121ab2b8be35410f1361fcd31dad9c9efca423
Kaspars Mickevics <kaspars@fx.lv>
2016-05-01 15:20:54 +0300

    Linters added to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-py.test --cov endpoint_check.py test_endpoint_check.py 
+echo "flake8"
+flake8 endpoint_check.py
+echo "pyflakes"
+pyflakes endpoint_check.py
+echo "pytest"
+py.test -v --cov endpoint_check.py test_endpoint_check.py 
