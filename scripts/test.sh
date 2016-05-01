ca3826b0eec9795cc9606a83ed99b69912b9a958
Kaspars Mickevics <kaspars@fx.lv>
2016-05-01 13:02:14 +0300

    Start doing tests

---
+++ scripts/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+py.test --cov endpoint_check.py test_endpoint_check.py 
---
+++ scripts/test_endpoint_check.py
@@ -0,0 +1,5 @@
+import endpoint_check
+def test_average():
+    assert endpoint_check.average((1,2,3,4,5)) == 3
+
+
